3f3a0adf73c45ff1f949cc0b3c351884599cb297
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-02-10 16:34:54 +1300

    Update Poseidon.lean dependency to a version using Mathlib types
    Also update examples/proofs that used `Spec.lean`. `Spec.lean` has been merged into the dependency Poseidon.lean.
    
    Poseidon.lean now uses Mathlib types for matrices and `ZMod` instead of Yatima equivalents.
    
    Poseidon.lean also now has `precompileModules` set to `false` which should result in faster builds.

--- Poseidon2Fv/Equivalence.lean
+++ Poseidon2Fv/Equivalence.lean
@@ -1,6 +1,6 @@
 import Mathlib
 
-import Poseidon2Fv.Spec
+import Poseidon.Hash
 import Poseidon2Fv.Folding
 
 lemma apply_m4_equiv
@@ -12,17 +12,17 @@ lemma apply_m4_equiv
   (h_state2 : state (offset + 2) = x2)
   (h_state3 : state (offset + 3) = x3)
 :
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3]).size = 4 ∧
+  (Poseidon2.smallMatrixAction #[x0, x1, x2, x3]).size = 4 ∧
   ∀ idx: Fin 4,
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3])[idx]? =
+  (Poseidon2.smallMatrixAction #[x0, x1, x2, x3])[idx]? =
   .some ↑(Poseidon2.Folding.apply_m4 state offset idx)
 := by
 
   simp [
-    Poseidon2Spec.smallMatrixAction,
+    Poseidon2.smallMatrixAction,
     Poseidon2.Folding.apply_m4,
-    Poseidon2Spec.matrix_action,
-    Poseidon2Spec.add_array
+    Poseidon2.matrix_action,
+    Poseidon2.add_array
   ]
   intro idx; fin_cases idx <;> {
     simp [
