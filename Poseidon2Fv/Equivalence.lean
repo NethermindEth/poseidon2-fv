2c1ccdd297d3f47ebc18087fa9198321b84e81ef
Dom Henderson <dom@nethermind.io>
2026-02-09 16:49:23 -0800

    Combine elementwise equiv lemmas

--- Poseidon2Fv/Equivalence.lean
+++ Poseidon2Fv/Equivalence.lean
@@ -3,110 +3,33 @@ import Mathlib
 import Poseidon2Fv.Spec
 import Poseidon2Fv.Folding
 
-lemma apply_m4_equiv_0
+lemma apply_m4_equiv
   [Fact P.Prime]
   (x0 x1 x2 x3: ZMod P) --mathlib zmod
   (state : Fin 16 → ZMod P)
-  (h_state0 : state idx = x0)
-  (h_state1 : state (idx + 1) = x1)
-  (h_state2 : state (idx + 2) = x2)
-  (h_state3 : state (idx + 3) = x3)
+  (h_state0 : state offset = x0)
+  (h_state1 : state (offset + 1) = x1)
+  (h_state2 : state (offset + 2) = x2)
+  (h_state3 : state (offset + 3) = x3)
 :
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3])[0]? =
-  .some ↑(Poseidon2.Folding.apply_m4 state idx 0)
+  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3]).size = 4 ∧
+  ∀ idx: Fin 4,
+  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3])[idx]? =
+  .some ↑(Poseidon2.Folding.apply_m4 state offset idx)
 := by
+
   simp [
     Poseidon2Spec.smallMatrixAction,
     Poseidon2.Folding.apply_m4,
-    h_state0,
-    h_state1,
-    h_state2,
-    h_state3,
     Poseidon2Spec.matrix_action,
     Poseidon2Spec.add_array
   ]
-  grind
-
-lemma apply_m4_equiv_1
-  [Fact P.Prime]
-  (x0 x1 x2 x3: ZMod P) --mathlib zmod
-  (state : Fin 16 → ZMod P)
-  (h_state0 : state idx = x0)
-  (h_state1 : state (idx + 1) = x1)
-  (h_state2 : state (idx + 2) = x2)
-  (h_state3 : state (idx + 3) = x3)
-:
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3])[1]? =
-  .some ↑(Poseidon2.Folding.apply_m4 state idx 1)
-:= by
-  simp [
-    Poseidon2Spec.smallMatrixAction,
-    Poseidon2.Folding.apply_m4,
-    h_state0,
-    h_state1,
-    h_state2,
-    h_state3,
-    Poseidon2Spec.matrix_action,
-    Poseidon2Spec.add_array
-  ]
-  grind
-
-lemma apply_m4_equiv_2
-  [Fact P.Prime]
-  (x0 x1 x2 x3: ZMod P) --mathlib zmod
-  (state : Fin 16 → ZMod P)
-  (h_state0 : state idx = x0)
-  (h_state1 : state (idx + 1) = x1)
-  (h_state2 : state (idx + 2) = x2)
-  (h_state3 : state (idx + 3) = x3)
-:
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3])[2]? =
-  .some ↑(Poseidon2.Folding.apply_m4 state idx 2)
-:= by
-  simp [
-    Poseidon2Spec.smallMatrixAction,
-    Poseidon2.Folding.apply_m4,
-    h_state0,
-    h_state1,
-    h_state2,
-    h_state3,
-    Poseidon2Spec.matrix_action,
-    Poseidon2Spec.add_array
-  ]
-  grind
-
-lemma apply_m4_equiv_3
-  [Fact P.Prime]
-  (x0 x1 x2 x3: ZMod P) --mathlib zmod
-  (state : Fin 16 → ZMod P)
-  (h_state0 : state idx = x0)
-  (h_state1 : state (idx + 1) = x1)
-  (h_state2 : state (idx + 2) = x2)
-  (h_state3 : state (idx + 3) = x3)
-:
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3])[3]? =
-  .some ↑(Poseidon2.Folding.apply_m4 state idx 3)
-:= by
-  simp [
-    Poseidon2Spec.smallMatrixAction,
-    Poseidon2.Folding.apply_m4,
-    h_state0,
-    h_state1,
-    h_state2,
-    h_state3,
-    Poseidon2Spec.matrix_action,
-    Poseidon2Spec.add_array
-  ]
-  grind
-
-lemma apply_m4_equiv_size
-  [Fact P.Prime]
-  (x0 x1 x2 x3: ZMod P) --mathlib zmod
-:
-  (Poseidon2Spec.smallMatrixAction #[x0, x1, x2, x3]).size = 4
-:= by
-  simp [
-    Poseidon2Spec.smallMatrixAction,
-    Poseidon2Spec.matrix_action,
-    Poseidon2Spec.add_array
-  ]
+  intro idx; fin_cases idx <;> {
+    simp [
+      h_state0,
+      h_state1,
+      h_state2,
+      h_state3,
+    ]
+    grind
+  }
